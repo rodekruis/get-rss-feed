0fefe55e6dd33e66724e9945bfca5d5a19e2e752
jmargutti <jacopo.margutti@gmail.com>
2023-03-05 16:38:50 +0100

    repurpose for Syria

--- pipeline/src/pipeline/pipeline.py
+++ pipeline/src/pipeline/pipeline.py
@@ -4,6 +4,7 @@ from google.cloud import translate_v2 as google_translate
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from transformers import pipeline
+import tweepy
 import pandas as pd
 from tqdm import tqdm
 import re
@@ -30,6 +31,30 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("azure").setLevel(logging.WARNING)
 logging.getLogger("requests_oauthlib").setLevel(logging.WARNING)
+
+
+def get_url_from_entities(entities):
+    try:
+        return entities['urls'][0]['expanded_url']
+    except:
+        return np.nan
+
+
+def get_url_from_tweet(row):
+    return f"https://twitter.com/{row['screen_name']}/status/{row['id']}"
+
+
+def format_df(df_tweets):
+    df_tweets['user'] = df_tweets['user'].astype(str).apply(ast.literal_eval)
+    df_tweets['source'] = df_tweets['user'].apply(lambda x: x['name'])
+    df_tweets['screen_name'] = df_tweets['user'].apply(lambda x: x['screen_name'])
+    df_tweets['entities'] = df_tweets['entities'].astype(str).apply(ast.literal_eval)
+    df_tweets['url'] = df_tweets['entities'].apply(get_url_from_entities)
+    df_tweets['twitter_url'] = df_tweets.apply(get_url_from_tweet, axis=1)
+    df_tweets['url'] = df_tweets['url'].fillna(df_tweets['twitter_url'])
+    df_tweets = df_tweets[['created_at', 'id', 'full_text', 'source', 'geo', 'coordinates', 'place', 'retweet_count',
+                           'favorite_count', 'possibly_sensitive', 'lang', 'url']]
+    return df_tweets
 
 
 def main():
@@ -70,9 +95,6 @@ def main():
 
         # data sources
         sources = {
-            'Sana (Arabic)': 'https://sana.sy/?feed=rss2',
-            'Sana (English)': 'https://sana.sy/en/?feed=rss2',
-            'Alwatan': 'https://alwatan.sy/feed',
             'Alahednews': 'https://www.alahednews.com.lb/rss/',
             'Enab Baladi': 'https://www.enabbaladi.net/feed',
             'Daily Sabah': 'https://www.dailysabah.com/rssFeed/home-page',
@@ -84,10 +106,11 @@ def main():
 
         english_query = ["Syrian Arab Red Crescent", "Syrian Red Crescent", "Khaled Hboubati", "Khaled Erksoussi",
                          "Hossam Elsharkawi", "Mey Al Sayegh", "Idlib", "Idleb", "Safe access", "sanctions",
-                         "cross-border aid", "crossline operations", "cholera", "north-west Syria", "northwest Syria"]
+                         "cross-border aid", "crossline operations", "cholera", "north-west Syria", "northwest Syria",
+                         "quake", "earthquake"]
         arabic_query = ["إدلب", "العبور الآمن", " عقوبات", "مساعدات عبر الحدود", "العمليات عبر الحدود", "الكوليرا",
                         "شمالي غربي سوريا", "إدلب", "العبور الآمن", " عقوبات", "مساعدات عبر الحدود",
-                        "العمليات عبر الحدود", "الكوليرا", "شمالي غربي سوريا"]
+                        "العمليات عبر الحدود", "الكوليرا", "هزة أرضية", "شمالي غربي سوريا"]
         keywords = english_query + arabic_query
 
         entries = []
@@ -174,20 +197,119 @@ def main():
                 valueInputOption="USER_ENTERED", insertDataOption="INSERT_ROWS", body=body).execute()
             sleep(1)
 
-        # Twitter ...
-        twitter = {
-            'HashtagSyria': 'presshashtag',
-            'Almasdar Online': 'AlmasdaronlineE',
-            'Alghad': 'AlghadNews',
-            'Shaam': 'ShaamNetwork',
-            'Syrian Observatory for Human Rights': 'syriahr',
-            'Baladi News': 'baladinetwork',
-            'North Press Agency': 'NPA_Arabic',
-            'Sky News Arabia': 'skynewsarabia',
-            'Al Maydeen': 'Almayadeennews',
-            'Monte Carlo Doualiya': 'MC_Doualiya',
-            'BBC Arabic': 'BBCArabic'
-        }
+            # Twitter
+            twitter_sources = {
+                'SANA Syria': 'SANAEnOfficial',
+                'Alwatan Syria': 'AlwatanSy',
+                'HashtagSyria': 'presshashtag',
+                'Almasdar Online': 'AlmasdaronlineE',
+                'Alghad': 'AlghadNews',
+                'Shaam': 'ShaamNetwork',
+                'Syrian Observatory for Human Rights': 'syriahr',
+                'Baladi News': 'baladinetwork',
+                'North Press Agency': 'NPA_Arabic',
+                'Sky News Arabia': 'skynewsarabia',
+                'Al Maydeen': 'Almayadeennews',
+                'Monte Carlo Doualiya': 'MC_Doualiya',
+                'BBC Arabic': 'BBCArabic'
+            }
+
+            spreadsheet_id = '1p8zMlaXlC-3BpPbl5Yb61u6VZRUIxD1Gc2yo7PJ9ScY'
+            spreadsheet_range = 'Tweets!A:L'
+            # get data already in the spreadsheet
+            result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id,
+                                                         range=spreadsheet_range).execute()
+            values = result.get('values', [])
+            df_old_values = pd.DataFrame.from_records(values[1:], columns=values[0])
+            df_old_values['created_at'] = pd.to_datetime(df_old_values['created_at'])
+
+            auth = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY'], os.environ['TWITTER_API_SECRET'])
+            auth.set_access_token(os.environ['TWITTER_ACCESS_TOKEN'], os.environ['TWITTER_ACCESS_SECRET'])
+            api = tweepy.API(auth, wait_on_rate_limit=True)
+
+            twitter_data_path = "../data"
+            os.makedirs(twitter_data_path, exist_ok=True)
+
+            # track individual twitter accounts
+            for source_name, source_id in twitter_sources.items():
+                # save output as
+                save_file = twitter_data_path + '/tweets_' + source_id + '.json'
+                tweets = api.user_timeline(
+                    screen_name=source_id,
+                    count=200,
+                    include_rts=False,
+                    tweet_mode='extended'
+                )
+
+                all_tweets = []
+                all_tweets.extend(tweets)
+                oldest_id = tweets[-1].id
+                while True:
+                    tweets = api.user_timeline(
+                        screen_name=source_id,
+                        count=200,
+                        include_rts=False,
+                        max_id=oldest_id - 1,
+                        tweet_mode='extended'
+                    )
+                    if len(tweets) == 0:
+                        break
+                    oldest_id = tweets[-1].id
+                    all_tweets.extend(tweets)
+
+                with open(save_file, 'a') as tf:
+                    for tweet in all_tweets:
+                        try:
+                            tf.write('\n')
+                            json.dump(tweet._json, tf)
+                        except Exception as e:
+                            logging.warning("Some error occurred, skipping tweet:")
+                            logging.warning(e)
+                            pass
+
+            # parse tweets and store in dataframe
+            df_tweets = pd.DataFrame()
+            for file in os.listdir(twitter_data_path):
+                if file.endswith('.json'):
+                    df_tweets_ = pd.read_json(os.path.join(twitter_data_path, file), lines=True)
+                    df_tweets = df_tweets.append(df_tweets_, ignore_index=True)
+
+            df_tweets['relevant'] = True
+            for ix, row in df_tweets.iterrows():
+                # skip if link already present in google sheet
+                if not df_old_values.empty:
+                    if row['url'] in df_old_values['url'].unique():
+                        df_tweets.at[ix, 'relevant'] = False
+
+                if row['created_at'].dt.date < datetime.date.fromisoformat('2023-02-06'):
+                    df_tweets.at[ix, 'relevant'] = False
+
+                # filter by location
+                if not ('syria' in row['full_text'] or 'سوريا' in row['full_text']):
+                    df_tweets.at[ix, 'relevant'] = False
+
+                # filter by keyword
+                if not any(keyword.lower() in row['full_text'].lower() for keyword in keywords):
+                    df_tweets.at[ix, 'relevant'] = False
+            df_tweets = df_tweets[df_tweets['relevant']].drop(columns=['relevant'])
+
+            # drop duplicates
+            df_tweets = df_tweets.drop_duplicates(subset=['id'])
+            df_tweets = format_df(df_tweets)
+            df_tweets['created_at'] = df_tweets['created_at'].dt.tz_localize(None)
+            df_tweets = df_tweets.sort_values(by='created_at')
+            df_tweets['created_at'] = df_tweets['created_at'].astype(str)
+            df_tweets = df_tweets.fillna('')
+
+            # add entries to google sheet
+            logging.info('updating Google sheet')
+            for ix, row in df_tweets.iterrows():
+                # add new row to google sheet
+                body = {'values': [list(row.values)]}
+                result = service.spreadsheets().values().append(
+                    spreadsheetId=spreadsheet_id, range=spreadsheet_range,
+                    valueInputOption="USER_ENTERED", insertDataOption="INSERT_ROWS", body=body).execute()
+                sleep(1)
 
     except Exception as e:
         logging.error(f"{e}")
