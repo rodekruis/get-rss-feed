f15002cd817d1254ba2edd291109eac9ad06cbfe
jmargutti <jacopo.margutti@gmail.com>
2023-03-05 15:20:02 +0100

    repurpose for Syria

--- pipeline/src/pipeline/pipeline.py
+++ pipeline/src/pipeline/pipeline.py
@@ -38,7 +38,6 @@ def main():
     utc_timestamp = (
         datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
     )
-    count_skipped, count_translated, count_saved = 0, 0, 0
 
     try:
         # initialize google sheets api
@@ -46,7 +45,8 @@ def main():
             'https://www.googleapis.com/auth/spreadsheets',
             'https://www.googleapis.com/auth/cloud-translation'
         ]
-        spreadsheet_id = '1QmjjbzDc_T91aMsZY7BQWod_fBJgYNhPYGsuHp5Zkz8'
+        spreadsheet_id = '1p8zMlaXlC-3BpPbl5Yb61u6VZRUIxD1Gc2yo7PJ9ScY'
+        spreadsheet_range = 'Articles!A:G'
         service_account_info = json.load(open(f"{credentials_path}/google-service-account-template.json"))
         service_account_info['private_key_id'] = os.environ['PRIVATE_KEY_ID']
         service_account_info['private_key'] = os.environ['PRIVATE_KEY'].replace(r'\n', '\n')
@@ -55,12 +55,12 @@ def main():
         service = build('sheets', 'v4', credentials=creds)
 
         # initialize the translator
-        tr_en_translator = google_translate.Client(credentials=creds)
-        # tr_en_translator = pipeline("translation_tr_to_en", model=f"Helsinki-NLP/opus-mt-tr-en")
+        translator = google_translate.Client(credentials=creds)
+        # translator = pipeline("translation_tr_to_en", model=f"Helsinki-NLP/opus-mt-tr-en")
 
         # get data already in the spreadsheet
         result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id,
-                                                     range='Turkiye Timeline of events!A:F').execute()
+                                                     range=spreadsheet_range).execute()
         values = result.get('values', [])
         df_old_values = pd.DataFrame.from_records(values[1:], columns=values[0])
         df_old_values['datetime'] = pd.to_datetime(
@@ -70,10 +70,26 @@ def main():
 
         # data sources
         sources = {
-            'Evrensel': 'https://www.evrensel.net/rss/haber.xml'
+            'Sana (Arabic)': 'https://sana.sy/?feed=rss2',
+            'Sana (English)': 'https://sana.sy/en/?feed=rss2',
+            'Alwatan': 'https://alwatan.sy/feed',
+            'Alahednews': 'https://www.alahednews.com.lb/rss/',
+            'Enab Baladi': 'https://www.enabbaladi.net/feed',
+            'Daily Sabah': 'https://www.dailysabah.com/rssFeed/home-page',
+            'Kurdpress': 'https://kurdpress.com/rss.php?lang=fa&cat=10',
+            'Al Jazeera': 'https://www.aljazeera.com/xml/rss/all.xml',
+            'Al Arabiya': 'https://www.alarabiya.net/feed/rss2/ar.xml',
+            'Middle East Monitor': 'https://www.middleeastmonitor.com/feed/'
         }
-        keywords = ['earthquake', 'victim', 'destruction', 'destroyed', 'damage', 'emergency', 'body', 'bodies', 'tent',
-                    'collapse', 'rubble', 'survive', 'survivors','turkish border']
+
+        english_query = ["Syrian Arab Red Crescent", "Syrian Red Crescent", "Khaled Hboubati", "Khaled Erksoussi",
+                         "Hossam Elsharkawi", "Mey Al Sayegh", "Idlib", "Idleb", "Safe access", "sanctions",
+                         "cross-border aid", "crossline operations", "cholera", "north-west Syria", "northwest Syria"]
+        arabic_query = ["إدلب", "العبور الآمن", " عقوبات", "مساعدات عبر الحدود", "العمليات عبر الحدود", "الكوليرا",
+                        "شمالي غربي سوريا", "إدلب", "العبور الآمن", " عقوبات", "مساعدات عبر الحدود",
+                        "العمليات عبر الحدود", "الكوليرا", "شمالي غربي سوريا"]
+        keywords = english_query + arabic_query
+
         entries = []
 
         for source_name, source_url in sources.items():
@@ -82,54 +98,69 @@ def main():
             NewsFeed = feedparser.parse(source_url)
 
             for entry in NewsFeed.entries:
-                datetime_entry = pd.to_datetime(entry['published'])
+                datetime = pd.to_datetime(entry['published'])
 
-                # skip if link already present in google sheet
-                if entry['link'] in df_old_values['Link'].unique():
-                    count_skipped += 1
-                    continue
-                # skip if older than latest news
-                if datetime_entry < df_old_values['datetime'].max().tz_localize('UTC+03:00'):
-                    count_skipped += 1
-                    continue
+                if not df_old_values.empty:
+                    # skip if link already present in google sheet
+                    if entry['link'] in df_old_values['Link'].unique():
+                        continue
+                    # skip if older than latest news
+                    if datetime < df_old_values[df_old_values['Source'] == source_name]['datetime'].max().tz_localize(
+                            'UTC+03:00'):
+                        print(f"{datetime} is older than {df_old_values['datetime'].max()}, skipping")
+                        continue
+                    else:
+                        print(f"{datetime} is newer than {df_old_values['datetime'].max()}, saving")
 
                 title = re.sub(r"<(.*)>", "", entry['title'])  # clean title (without HTML leftovers)
-                title_en = tr_en_translator.translate(title, target_language="en")["translatedText"]  # translate title to english
+                title_en = title  # translator.translate(title, target_language="en")["translatedText"]  # translate title to english
 
                 res = requests.get(entry['id'])
                 if res.status_code == 200:
                     soup = BeautifulSoup(res.content, 'html.parser')
-                    text = [tr_en_translator.translate(p.get_text(), target_language="en")["translatedText"] for p in soup.find_all('p')] # translate content
-                    content_en = ' '.join(text.copy())
+                    text = [p.get_text() for p in soup.find_all('p')]
+                    content = ' '.join(text.copy())
+                    text_en = text  # [translator.translate(x, target_language="en")["translatedText"] for x in text]
+                    content_en = ' '.join(text_en.copy())
                 elif 'summary' in entry.keys():
-                    summary = re.sub(r"<(.*)>", "", entry['summary'])  # clean summary (without HTML leftovers)
-                    content_en = tr_en_translator.translate(summary, target_language="en")["translatedText"]  # translate summary
+                    content = re.sub(r"<(.*)>", "", entry['summary'])  # clean summary (without HTML leftovers)
+                    content_en = content  # translator.translate(content, target_language="en")["translatedText"]
                 else:
+                    content = title
                     content_en = title_en
-                count_translated += 1
+
+                # filter by location
+                if not ('syria' in title_en.lower() or 'syria' in content_en.lower() or
+                        'سوريا' in title.lower() or 'سوريا' in content.lower()):
+                    logging.info('This entry is not about Syria:')
+                    logging.info(f"{title_en}")
+                    logging.info(f"{content_en}")
+                    logging.info('---------------------------------------')
+                    continue
 
                 # filter by keyword
                 if not any(keyword.lower() in title_en.lower() or keyword.lower() in content_en.lower() for keyword in
                            keywords):
-                    logging.info('This entry is not about the earthquake:')
+                    logging.info('This entry is not relevant:')
                     logging.info(f"{title_en}")
                     logging.info(f"{content_en}")
                     logging.info('---------------------------------------')
-                    count_skipped += 1
                     continue
 
                 # create simple entry
                 entry_simple = {
-                    'Date': datetime_entry.strftime("%d/%m/%Y"),
-                    'Time': datetime_entry.strftime("%H:%M"),
-                    'information': content_en,
+                    'Date': datetime.strftime("%d/%m/%Y"),
+                    'Time': datetime.strftime("%H:%M"),
+                    # 'Title (en)': title_en,
+                    'Title': title,
+                    # 'Content (en)': content_en,
+                    'Content': content,
                     'Source': source_name,
-                    'Source+datetime': f'{source_name}, {datetime_entry.strftime("%d/%m/%Y")} {datetime_entry.strftime("%H:%M")}',
+                    'Source+datetime': f'{source_name}, {datetime.strftime("%d/%m/%Y")} {datetime.strftime("%H:%M")}',
                     'Link': entry['link'],
-                    'datetime': datetime_entry
+                    'datetime': datetime
                 }
                 entries.append(entry_simple)
-                count_saved += 1
 
         entries_sorted = sorted(entries, key=lambda d: d['datetime'])  # sort entries by date (from oldest to newest)
 
@@ -139,16 +170,29 @@ def main():
             # add new row to google sheet
             body = {'values': [list(entry.values())[:-1]]}
             result = service.spreadsheets().values().append(
-                spreadsheetId=spreadsheet_id, range='Turkiye Timeline of events!A:F',
+                spreadsheetId=spreadsheet_id, range=spreadsheet_range,
                 valueInputOption="USER_ENTERED", insertDataOption="INSERT_ROWS", body=body).execute()
             sleep(1)
 
+        # Twitter ...
+        twitter = {
+            'HashtagSyria': 'presshashtag',
+            'Almasdar Online': 'AlmasdaronlineE',
+            'Alghad': 'AlghadNews',
+            'Shaam': 'ShaamNetwork',
+            'Syrian Observatory for Human Rights': 'syriahr',
+            'Baladi News': 'baladinetwork',
+            'North Press Agency': 'NPA_Arabic',
+            'Sky News Arabia': 'skynewsarabia',
+            'Al Maydeen': 'Almayadeennews',
+            'Monte Carlo Doualiya': 'MC_Doualiya',
+            'BBC Arabic': 'BBCArabic'
+        }
+
     except Exception as e:
         logging.error(f"{e}")
-        traceback.print_exception(*sys.exc_info())
 
-    logging.info("Pipeline ran at %s", utc_timestamp)
-    logging.info(f"{count_skipped} articles skipped, {count_translated} translated, {count_saved} saved")
+    logging.info("Python timer trigger function ran at %s", utc_timestamp)
 
 
 if __name__ == "__main__":
